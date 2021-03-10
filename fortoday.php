55c072086d22220d0e5d0889e2eade78f942f085
adnanebk88 <bakkouadnane88@gmail.com>
2021-03-10 18:56:37 +0100

    first commit

---
+++ fortoday.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <title>Document</title>
+</head>
+<body>
+<header id="navbar">
+        <div class="container">
+            <nav>
+                <a href="#" id="nav-btn"><i class="fas fa-bars"></i></a>
+                <a id="logo" href="#">CINEMA </a>
+                <ul class="nav-links">
+                    <li><a href="#" class="nav-link">F</a></li>
+                    <li><a href="#" class="nav-link">O</a></li>
+                    <li><a href="#" class="nav-link">R</a></li>
+                    <li><a href="#" class="nav-link">T</a></li>
+                    <li><a href="#" class="nav-link">O</a></li>
+                    <li><a href="#" class="nav-link">D</a></li>
+                    <li><a href="#" class="nav-link">A</a></li>
+                    <li><a href="#" class="nav-link">Y</a></li>
+                 
+                </ul>
+                <a href="#" id="cart-btn"><i class="fas fa-shopping-cart"></i></a>
+            </nav>  
+        </div>
+    </header>
+<section>
+    <div class="container">
+
+    
+        <table class="table">
+            <thead class="bg-danger">
+              <tr>
+                <th scope="col">H/S</th>
+                <th scope="col">SALLE 1</th>
+              
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <th scope="row">08H-10H</th>
+                <td> <input type="text" name="8-10salle1"></td>
+              
+              </tr>
+              <tr>
+                <th scope="row">10H-12H</th>
+                <td> <input type="text" name="10-12salle1" >  </td>
+              
+              </tr>
+              <tr>
+                <th scope="row">12H-14H</th>
+                <td> <input type="text" name="salle1" >  </td>
+                
+              </tr>
+              <tr>
+                <th scope="col">14H-16H</td>
+                <td> <input type="text" name="salle1" >  </td>
+         
+              </tr>
+           
+                  <tr>
+                <th scope="row">16H-18H</th>
+                <td> <input type="text" name="salle1" >  </td>
+              
+              </tr>
+              <tr>
+                <th scope="row">18H-20H</th>
+                <td> <input type="text" name="salle1" >  </td>
+         
+              </tr>
+              <tr>
+                <th scope="row">20H-22H</th>
+                <td> <input type="text" name="salle1" >  </td>
+           
+              </tr>
+              <tr>
+                <th scope="row">22H-00H</th>
+                <td> <input type="text" name="salle1" >  </td>
+                
+              </tr>
+            </tbody>
+          </table>
+          <div class="col text-center">
+         <button class="btn btn-success text-center">ENREGISTRER</button>
+          </div></div>
+    </section>  
+</body>
+</html>
